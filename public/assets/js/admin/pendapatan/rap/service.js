365ce3f5db44791342bfc7efc8eb26d6d7b801ea
zayn1510 <zayndevs.1510@gmail.com>
2024-04-08 21:52:26 +0800

    add token in service

--- public/assets/js/admin/pendapatan/rap/service.js
+++ public/assets/js/admin/pendapatan/rap/service.js
@@ -89,6 +89,50 @@ app.service("service", ["$http", function ($http) {
         });
     }
 
+    this.save_rap = function (obj, callback) {
+        $http({
+            url: URL_API + "rap/",
+            method: "POST",
+            data: obj,
+            headers: {
+                'Authorization': 'Bearer ' + accessToken // Attach the access token as a Bearer token
+            }
+        }).then(function successCallback(e) {
+            callback(e.data);
+        }).catch(function (err) {
+            callback(err);
+        });
+    }
+
+    this.update_rap = function (obj, id, callback) {
+        $http({
+            url: URL_API + "rap/" + id,
+            method: "PUT",
+            data: obj,
+            headers: {
+                'Authorization': 'Bearer ' + accessToken // Attach the access token as a Bearer token
+            }
+        }).then(function successCallback(e) {
+            callback(e.data);
+        }).catch(function (err) {
+            callback(err);
+        });
+    }
+
+    this.delete_rap = function (id, callback) {
+        $http({
+            url: URL_API + "rap/" + id,
+            method: "DELETE",
+            headers: {
+                'Authorization': 'Bearer ' + accessToken // Attach the access token as a Bearer token
+            }
+        }).then(function successCallback(e) {
+            callback(e.data);
+        }).catch(function (err) {
+            callback(err);
+        });
+    }
+
     this.get_anggaran_tahun = function (callback) {
         $http({
             url: URL_API + "anggaran-tahun/",
