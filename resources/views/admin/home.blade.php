34da172a3584439b0f34649454a2efed62f317fc
zayn1510 <zayndevs.1510@gmail>
2024-12-02 20:16:10 +0800

    update the dashboard

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -15,58 +15,7 @@
         <!-- sales report area start -->
         <div class="sales-report-area mt-5 mb-5">
             <div class="row">
-                <div class="col-12">
-                    <div class="row" style="margin-bottom: 10px;">
-                        <div class="col-md-4" style="cursor: pointer"
-                            onclick="window.location.href='{{ url('admin/page/ruangan') }}'">
-                            <div class="single-report">
-                                <div class="s-report-inner pr--10 pt--30 mb-3">
-                                    <div class="icon"><i class="ti-room"></i></div>
-                                    <div class="s-report-title d-flex justify-content-between">
-                                        <h4 class="header-title mb-0">Jabatan</h4>
-
-                                    </div>
-                                    <div class="d-flex justify-content-between pb-2">
-                                        <h2>12</h2>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="col-md-4" style="cursor: pointer"
-                            onclick="window.location.href='{{ url('admin/page/mapel') }}'">
-                            <div class="single-report">
-                                <div class="s-report-inner pr--10 pt--30 mb-3">
-                                    <div class="icon"><i class="ti-book"></i></div>
-                                    <div class="s-report-title d-flex justify-content-between">
-                                        <h4 class="header-title mb-0">Perangkat Desa</h4>
-
-                                    </div>
-                                    <div class="d-flex justify-content-between pb-2">
-                                        <h2>12</h2>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                        <div class="col-md-4" style="cursor: pointer"
-                            onclick="window.location.href='{{ url('admin/page/orangtua') }}'">
-                            <div class="single-report">
-                                <div class="s-report-inner pr--10 pt--30 mb-3">
-                                    <div class="icon"><i class="ti-user"></i></div>
-                                    <div class="s-report-title d-flex justify-content-between">
-                                        <h4 class="header-title mb-0">Pengguna</h4>
-
-                                    </div>
-                                    <div class="d-flex justify-content-between pb-2">
-                                        <h2>12</h2>
-                                    </div>
-                                </div>
-
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                
                 <div class="col-12">
 
                     <div class="row">
